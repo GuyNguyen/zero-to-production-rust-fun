8463189d70e81e14339a6575fc8f0d976735d3f4
Guy Nguyen-Phuoc <nguyen46@hawaii.edu>
2023-03-05 04:30:28 -1000

    library for main.rs

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new( || {
+            App::new()
+                .route("/", web::get().to(greet))
+                .route("/hello/{name}", web::get().to(greet))
+                .route("/health_check", web::get().to(health_check))
+        })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
